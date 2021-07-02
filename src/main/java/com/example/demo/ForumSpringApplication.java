2fa3af725727d62ebb049a083a41124b0cdb0fd2
Beiko14 <heiko.hollerith14@gmx.de>
2021-07-02 13:54:37 +0200

    added necessary models

--- src/main/java/com/example/demo/ForumSpringApplication.java
+++ src/main/java/com/example/demo/ForumSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ForumSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ForumSpringApplication.class, args);
 	}
 
 }
