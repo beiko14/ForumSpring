58549cf434588fe7b158ccc8242f6bed68379fb5
Beiko14 <heiko.hollerith14@gmx.de>
2021-07-08 14:40:49 +0200

    fixed typo in the subforum api path

--- src/main/java/com/example/demo/controller/SubForumController.java
+++ src/main/java/com/example/demo/controller/SubForumController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/subForum")
+@RequestMapping("/api/subforum")
 @AllArgsConstructor
 @Slf4j
 public class SubForumController {
